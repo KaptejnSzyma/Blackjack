0c111775c056b86c538dc0c277707e6e4e04d49f
Szymek <szymon.rzadca@gmail.com>
2018-06-04 10:17:43 +0200

    further frames setup

--- blackjack.py
+++ blackjack.py
@@ -20,3 +20,7 @@ card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background='green', fg='white').grid(row=0, column=0)
 tkinter.Label(card_frame, text=dealer_score_label, background='green', fg='white').grid(row=1, column=0)
+
+player_score_label = tkinter.IntVar
+tkinter.Label(card_frame, text='Player', background='green', fg='white').grid(row=2, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background='green', fg='white').grid(row=3, column=0)
