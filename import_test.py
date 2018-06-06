e0171cd2c35ab96a707d17004c0bccabf2a86e34
Szymek <szymon.rzadca@gmail.com>
2018-06-06 10:48:20 +0200

    messing up with modules

--- import_test.py
+++ import_test.py
@@ -1,10 +1,4 @@
 import blackjack
 
-g = sorted(globals())
-for x in g:
-    print(x)
-# print(__name__)
-# blackjack._deal_card(blackjack.dealer_card_frame)
-# blackjack.play()
-# print(blackjack.cards)
 blackjack._deal_card(blackjack.dealer_card_frame)
+blackjack.play()
